339eaedde923ea27fd6558449591235a4d81bcfe
iamkijun <leeguain@gmail.com>
2022-09-09 22:17:25 +0900

    0909

---
+++ BOJ/Recursion/1003.py
@@ -0,0 +1,15 @@
+
+import sys
+sys.stdin = open('Recursion/input.txt','r')
+
+for t in range(int(input())):
+
+    cnt_0 = [1, 0]
+    cnt_1 = [0, 1]
+    n = int(input())
+    if n> 1:
+        for i in range(n-1):
+            cnt_0.append(cnt_1[-1])
+            cnt_1.append(cnt_0[-2]+cnt_1[-1]) 
+ 
+    print(cnt_0[n], cnt_1[n])
