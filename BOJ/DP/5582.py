7b45830d12064f7b043e8ed3fd589e94aff09513
iamkijun <leeguain@gmail.com>
2022-10-20 23:37:51 +0900

    1020

--- BOJ/DP/5582.py
+++ BOJ/DP/5582.py
@@ -2,11 +2,15 @@ import sys
 sys.stdin = open('DP/input.txt','r')
 
 '''
-5
-1 2 3 4 2
+ABRACADABRA
+ECADADABRBCRDARA
 '''
 # 5582 -> 9251 -> 1695
 
 #제한된 선택지에서 선택하는 문제는, DP로 접근하자.
 
-N = int(input())
+s = input()
+t = input()
+
+for i in range(len(s)):
+    
